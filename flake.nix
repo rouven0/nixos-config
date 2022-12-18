9e4582d3512f8613b0aac4988a39ac3036bdf543
Rouven Seifert <rouven@rfive.de>
2022-12-18 17:01:34 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "My hardware setup";
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs/nixos-22.11;
+    home-manager.url = github:nix-community/home-manager;
+  };
+
+  outputs = { self, nixpkgs, home-manager }: {
+    nixosConfigurations = {
+      thinkpad = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/thinkpad/configuration.nix
+        ];
+      };
+    };
+  };
+}
