f85c5de8b7cba25dde798f0f2102ca7736937757
Rouven Seifert <rouven@rfive.de>
2022-12-27 23:44:46 +0100

    let home manager follow nixpkgs I have no clue what that means tbh

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
   inputs = {
     nixpkgs.url = github:nixos/nixpkgs/nixos-22.11;
     home-manager.url = github:nix-community/home-manager;
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     sops-nix.url = github:Mic92/sops-nix;
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
