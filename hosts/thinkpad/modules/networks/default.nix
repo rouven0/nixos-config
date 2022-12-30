ad7938a056b886e294687e05205e4c6f49944529
Rouven Seifert <rouven@rfive.de>
2022-12-30 11:02:10 +0100

    added dorm wireguard

--- hosts/thinkpad/modules/networks/default.nix
+++ hosts/thinkpad/modules/networks/default.nix
@@ -2,12 +2,15 @@
 {
   imports = [ ./uni.nix ];
 
-  sops.secrets."wireless-env" = { };
+  sops.secrets = {
+    "wireless-env" = { };
+    "wireguard/dorm/private" = { };
+    "wireguard/dorm/preshared" = { };
+  };
   networking = {
     hostName = "thinkpad";
     firewall = {
-      allowedUDPPorts = [ 51820 ]; # used for wireguard
-      checkReversePath = false;
+      #allowedUDPPorts = [ 51820 ]; # used for wireguard
     };
     wireless = {
       enable = true;
@@ -25,6 +28,23 @@
             disabled=1
           '';
         };
+      };
+    };
+    wg-quick.interfaces = {
+      Dorm = {
+        address = [ "10.10.10.3/32" ];
+        privateKeyFile = config.sops.secrets."wireguard/dorm/private".path; 
+        listenPort = 51820;
+        dns = [ "192.168.10.1" ];
+        autostart = false;
+        peers = [
+          {
+            publicKey = "vUmworuJFHjB4KUdkucQ+nzqO2ysARLomq4UuK1n430=";
+            presharedKeyFile = config.sops.secrets."wireguard/dorm/preshared".path; 
+            allowedIPs = [ "0.0.0.0/0" ];
+            endpoint = "dorm.vpn.rfive.de:51820";
+          }
+        ];
       };
     };
   };
