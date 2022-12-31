ac3b4fcbb475707ec2f90159841a4cb2b5171342
Rouven Seifert <rouven@rfive.de>
2022-12-31 10:52:13 +0100

    added wallpaper

--- users/rouven/modules/awesome/default.nix
+++ users/rouven/modules/awesome/default.nix
@@ -1,5 +1,6 @@
 { config, pkgs, ... }:
 {
   # declaration of awesome wm is in hosts/<name>/default.nix
+  home.file.".wallpaper.jpg".source = ../../../../images/wallpaper.jpg;
   xdg.configFile."awesome/rc.lua".source = ./rc.lua;
 }
