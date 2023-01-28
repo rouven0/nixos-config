c8c4be8737a9169d6fea4701c618d1f99b581bb0
Rouven Seifert <rouven@rfive.de>
2023-01-28 15:26:54 +0100

    console colors

--- hosts/thinkpad/default.nix
+++ hosts/thinkpad/default.nix
@@ -24,6 +24,25 @@
   console = {
     keyMap = "dvorak";
     font = "Lat2-Terminus16";
+    colors = [
+      # hardcoded dracula since nix-colors only provides a home manager module
+      "282936"
+      "3a3c4e"
+      "4d4f68"
+      "626483"
+      "62d6e8"
+      "e9e9f4"
+      "f1f2f8"
+      "f7f7fb"
+      "ea51b2"
+      "b45bcf"
+      "00f769"
+      "ebff87"
+      "a1efe4"
+      "62d6e8"
+      "b45bcf"
+      "00f769"
+    ];
   };
 
   fonts.fonts = with pkgs; [
