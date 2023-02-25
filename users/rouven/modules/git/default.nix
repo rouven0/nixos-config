a06384c5d3e8a847bbc9e0e2a21cb838cea06d42
Rouven Seifert <rouven@rfive.de>
2023-02-25 23:59:19 +0100

    configured delta for git

--- users/rouven/modules/git/default.nix
+++ users/rouven/modules/git/default.nix
@@ -1,10 +1,21 @@
 { config, pkgs, ... }:
 {
+  home.packages = with pkgs; [ delta ];
   programs.git = {
     enable = true;
     userName = "Rouven Seifert";
     userEmail = "rouven@rfive.de";
     extraConfig = {
+      core.pager = "delta";
+      interactive.diffFilter = "delta --color-only";
+      delta = {
+        navigate = true;
+        light = false;
+        side-by-side = true;
+        line-numbers = true;
+      };
+      merge.conflictStyle = "diff3";
+      diff.colorMoved = "default";
       user.signingkey = "B95E8FE6B11C4D09";
       pull.rebase = false;
       init.defaultBranch = "main";
