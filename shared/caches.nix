a20844be17e27dfc40d770736c14b4207cea46b5
Rouven Seifert <rouven@rfive.de>
2023-04-12 10:21:28 +0200

    added my own cache

--- shared/caches.nix
+++ shared/caches.nix
@@ -1,7 +1,13 @@
 { ... }:
 {
   nix.settings = {
-    substituters = [ "https://hyprland.cachix.org" ];
-    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+    substituters = [
+      "https://hyprland.cachix.org"
+      "https://cache.rfive.de"
+    ];
+    trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "cache.rfive.de:2E/yzJduGj4SJqYqDhpXO7aM2m5buMMUHN64EZdml3I="
+    ];
   };
 }
