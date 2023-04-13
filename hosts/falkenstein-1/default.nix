e19d6002f0a5d334a2c02b41173784fd444f5532
Rouven Seifert <rouven@rfive.de>
2023-04-13 14:28:09 +0200

    forgot to import

--- hosts/falkenstein-1/default.nix
+++ hosts/falkenstein-1/default.nix
@@ -7,6 +7,7 @@
       ./hardware-configuration.nix
       ./modules/networks
       ./modules/nginx
+      ./modules/purge
     ];
 
   boot = {
--- hosts/falkenstein-1/modules/purge/default.nix
+++ hosts/falkenstein-1/modules/purge/default.nix
@@ -12,7 +12,7 @@ in
     enableACME = true;
     forceSSL = true;
     locations."/" = {
-      proxyPass = "http://127.0.0.1:${toString config.services.purge.port}";
+      proxyPass = "http://127.0.0.1:${toString config.services.purge.listenPort}";
     };
   };
 }
