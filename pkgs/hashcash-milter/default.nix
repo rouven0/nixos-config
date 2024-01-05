34197679170ab2b70484c6308ab5a5ef882b7687
Rouven Seifert <rouven@rfive.de>
2024-01-05 12:03:53 +0100

    hashcash-milter: fix build

--- pkgs/hashcash-milter/default.nix
+++ pkgs/hashcash-milter/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, lib }:
+{ stdenv, fetchFromGitHub, lib, libmilter, db }:
 stdenv.mkDerivation rec {
   pname = "hashcash-milter";
   version = "0.1.3";
@@ -8,6 +8,11 @@ stdenv.mkDerivation rec {
     rev = "v${version}";
     hash = "sha256-yVpfvwpZUZQppZpmXmAqjoZH5shWUnA8aMVSOkPyQXw=";
   };
+  buildInputs = [ libmilter db ];
+  installPhase = ''
+    mkdir -p $out/bin
+    install -m 755 -p -s hashcash-milter $out/bin
+  '';
 
   meta = with lib; {
     description = "Hashcash Milter";
