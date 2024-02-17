d7475bf5601bdc7439a8999fb8a1ff468bc93401
Rouven Seifert <rouven@rfive.de>
2024-02-17 19:13:52 +0100

    git: use native delta options and configure sendmail

--- users/rouven/modules/git/default.nix
+++ users/rouven/modules/git/default.nix
@@ -4,21 +4,31 @@
     enable = true;
     userName = "Rouven Seifert";
     userEmail = "rouven@rfive.de";
-    extraConfig = {
-      core.pager = "${pkgs.delta}/bin/delta";
-      interactive.diffFilter = "${pkgs.delta}/bin/delta --color-only";
-      delta = {
+    delta = {
+      enable = true;
+      options = {
+        features = "decorations";
         navigate = true;
         light = false;
         side-by-side = true;
         line-numbers = true;
       };
+    };
+    extraConfig = {
       merge.conflictStyle = "diff3";
       diff.colorMoved = "default";
       user.signingkey = "B95E8FE6B11C4D09";
       pull.rebase = false;
       init.defaultBranch = "main";
       commit.gpgsign = true;
+      sendemail = {
+        from = "Rouven Seifert <rouven@rfive.de>";
+        smtpEncryption = "ssl";
+        smtpServer = "mail.rfive.de";
+        smteServerPort = 465;
+        smtpSslCertPath = "/etc/ssl/certs/ca-certificates.crt";
+        smtpUser = "rouven";
+      };
     };
   };
   programs.gh = {
